6340217b0226ac19b70031fa0f22148131e4c24d
jerrover <jerrover@gmail.com>
2025-09-10 13:47:17 +0700

    Commit No. 3 – Fix application crash when user input wrong Color code [Intent]

--- app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
@@ -5,14 +5,31 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
+
     private val submitButton: Button
         get() = findViewById(R.id.submit_button)
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                activityResult ->
+            val data = activityResult.data
+            val error = data?.getBooleanExtra(ERROR_KEY, false)
+            if(error == true){
+                Toast
+                    .makeText(this, getString(R.string.color_code_input_invalid),
+                        Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,16 +39,18 @@ class MainActivity : AppCompatActivity() {
             if(colorCode.isNotEmpty()){
                 if (colorCode.length < 6){
                     Toast
-                        .makeText(this,
-                            getString(R.string.color_code_input_wrong_length), Toast.LENGTH_LONG)
+                        .makeText(this, getString(R.string.color_code_input_wrong_length),
+                            Toast.LENGTH_LONG)
                         .show()
                 }
                 else{
                     val ResultIntent = Intent(this, ResultActivity::class.java)
                     ResultIntent.putExtra(COLOR_KEY, colorCode)
-                    startActivity(ResultIntent)
+                    //startActivity(ResultIntent)
+                    startForResult.launch(ResultIntent)
                 }
             }
+
             else{
                 Toast
                     .makeText(this, getString(R.string.color_code_input_empty),
